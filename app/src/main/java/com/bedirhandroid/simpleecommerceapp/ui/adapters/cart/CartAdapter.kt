b5eb6c1df35625c34f54c8a74e8c748154be4b52
Bedirhan Dag <bdrhandag@gmail.com>
2023-10-28 14:23:10 +0300

    Updated CartAdapter.kt

--- app/src/main/java/com/bedirhandroid/simpleecommerceapp/ui/adapters/cart/CartAdapter.kt
+++ app/src/main/java/com/bedirhandroid/simpleecommerceapp/ui/adapters/cart/CartAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bedirhandroid.simpleecommerceapp.databinding.CartRowBinding
 import com.bedirhandroid.simpleecommerceapp.network.models.uiresponses.product.ProductResponseUi
+import com.bedirhandroid.simpleecommerceapp.util.loadImage
 
 class CartAdapter(
     private val itemList: ArrayList<ProductResponseUi>,
@@ -25,6 +26,10 @@ class CartAdapter(
     override fun onBindViewHolder(holder: CartListVH, position: Int) {
         val item = itemList[position]
         holder.binding.apply {
+            item.image?.let { ivProduct.loadImage(it) }
+            tvProductTitle.text = item.title
+            tvPrice.text = item.price
+            item.rating?.let { rbRate.rating = it }
             ivProduct.setOnClickListener {
                 clickItem.invoke(item)
             }
